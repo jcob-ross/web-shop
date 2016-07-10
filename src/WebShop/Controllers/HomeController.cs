a429d946cb6d495047309e86c3b30507a3d62c31
jcob ross <rossler.jakub@email.cz>
2016-07-10 19:23:05 +0200

    removed client libs/bower

--- src/WebShop/Controllers/HomeController.cs
+++ src/WebShop/Controllers/HomeController.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebShop.Controllers
+﻿namespace WebShop.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+  using Microsoft.AspNetCore.Mvc;
 
-        public IActionResult Error()
-        {
-            return View();
-        }
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
     }
+
+    public IActionResult Error()
+    {
+      return View();
+    }
+  }
 }
